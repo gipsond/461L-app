e57fba7d8a28ad1153623994865aaede01dfbcdc
DESKTOP-TCOQQLT\jonat <jonathanmounsif@utexas.edu>
2019-02-19 01:28:04 -0600

    Added AysncTask class needed to request / execute the FullContact API query

--- ResYouMe/app/src/main/java/com/example/resyoume/CompanyInfo.java
+++ ResYouMe/app/src/main/java/com/example/resyoume/CompanyInfo.java
@@ -1,13 +1,16 @@
 package com.example.resyoume;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.StringEntity;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,23 +21,70 @@ import java.io.UnsupportedEncodingException;
 public class CompanyInfo extends AppCompatActivity {
 
     EditText CompanyDomainName;
-    TextView CompanyInfo;
-
+    TextView CompanyInfo_TextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_info);
-        CompanyDomainName = (EditText) findViewById(R.id.SendText);
-        CompanyInfo = (TextView) findViewById(R.id.ReceiveText);
+        CompanyDomainName = (EditText) findViewById(R.id.CompanyDomainName);
+        CompanyInfo_TextView = (TextView) findViewById(R.id.CompanyInfo);
     }
 
-    public void onClickInfo(View view) throws IOException {
-        Content response =  Request.Post("https://api.fullcontact.com/v3/company.enrich").addHeader("Authorization", "Bearer Kom1m2ezqBt2n2P46bo4dEw0uuZCNAIT").
-                 body(new StringEntity("{" + "\"domain\":\"microsoft.com\"" + "}")).execute().returnContent();
-        try {
-            JSONObject compnayInfo = new JSONObject(response.asString());
-        } catch (JSONException e) {
-            e.printStackTrace();
+    private class CompanyInfoAsyncTask extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+            Toast.makeText(CompanyInfo.this, "Company Information Loaded!", Toast.LENGTH_SHORT).show();
+            String CompanyName = null, Location = null, Twitter = null;
+            try {
+                CompanyName = jsonObject.getString("name");
+                Location = jsonObject.getString("location");
+                Twitter = jsonObject.getString("twitter");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            CompanyInfo_TextView.setText("Company Name: " + CompanyName +
+                                        "\nCompany Location: " + Location +
+                                        "\nCompany Twitter: " + Twitter);
         }
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            Request request = null;
+            Response response = null;
+            Content content = null;
+            JSONObject companyInfo_JSON = null;
+            try {
+                request = Request.Post("https://api.fullcontact.com/v3/company.enrich")
+                        .addHeader("Authorization","Bearer Kom1m2ezqBt2n2P46bo4dEw0uuZCNAIT")
+                        .body(new StringEntity("{" + "\"domain\":\"ni.com\"" + "}"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            try {
+                assert request != null;
+                response = request.execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                assert response != null;
+                content = response.returnContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                assert content != null;
+                companyInfo_JSON = new JSONObject(content.asString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return companyInfo_JSON;
+        }
+    }
+    // Creates an AsyncTask that does the company information lookup
+    public void onClickInfo(View view) throws NullPointerException {
+        CompanyInfoAsyncTask task = new CompanyInfoAsyncTask();
+        task.execute(""); // TODO: use the string to pass company domain name
     }
 }
