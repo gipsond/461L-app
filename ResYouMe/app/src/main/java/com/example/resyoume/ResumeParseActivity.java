751e08b733b4681bf8a8f1d9780cc69cdcb4388b
Errol Williams II <Errolwilliamsii@gmail.com>
2019-03-01 14:33:19 -0600

    Updated Parse Activity to allow uploads and save selected file to app/files/resumes folder

--- ResYouMe/app/src/main/java/com/example/resyoume/ResumeParseActivity.java
+++ ResYouMe/app/src/main/java/com/example/resyoume/ResumeParseActivity.java
@@ -2,9 +2,20 @@ package com.example.resyoume;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.util.Base64;
 import android.widget.Toast;
@@ -19,29 +30,73 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ResumeParseActivity extends AppCompatActivity {
 
     private static final String filename = "cv.pdf";
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> resumes=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    File resume_dir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_parse);
+
+        resume_dir = getBaseContext().getExternalFilesDir("resumes");
+        if (!resume_dir.exists()) {
+            if (resume_dir.mkdir()) ; //directory is created;
+        }
+
+        File[] files = resume_dir.listFiles();
+        for(File file: files){
+            resumes.add(file.getName());
+        }
+
+        ListView listView = (ListView)findViewById(R.id.resume_list);
+
+        adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_resume_parse, resumes);
+
+    }
+
+    public void add_resume(String resume_name) {
+        resumes.add(resume_name);
+        adapter.notifyDataSetChanged();
     }
 
     public void parse_resume(View view){
-        final TextView textView = (TextView) findViewById(R.id.parse_text);
+        final TextView textView = (TextView) findViewById(R.id.parse_resume_button);
         // ...
 
         // Instantiate the RequestQueue.
@@ -288,5 +343,68 @@ public class ResumeParseActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
+    }
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    private static final int GET_REQUEST_CODE = 42; //Code to ensure picker was used for GET_REQUEST
+
+    private static String[] mimeTypes = {
+            "application/msword",  //.doc
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+            "application/pdf" //pdf
+    };
+
+    public void upload_resume(View view){
+
+            // ACTION_GET_DOCUMENT is the intent to choose a file via the system's file browser
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+            // Filter to only show results that can be "opened", such as a
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+            // Filter to show only .doc,.docx, and pdf files using the image MIME data type.
+            intent.setType("*/*");
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+            startActivityForResult(intent, GET_REQUEST_CODE);
+    }
+
+    /**
+     * callback function after user selects document
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_GET_DOCUMENT intent was sent with the request code
+        // GET_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == GET_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = resultData.getData();
+            if (uri != null) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(uri);
+                    if(in != null) {
+                        byte[] bytes = IOUtils.toByteArray(in);
+                        String filename = resume_dir.getPath() + "/resume_" + uri.getLastPathSegment() + ".pdf";
+                        File destination = new File(filename);
+                        FileOutputStream out = new FileOutputStream(destination.getPath());
+                        out.write(bytes);
+                        add_resume(filename);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
