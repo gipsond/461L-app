87f9ab3f87524a829eaea0881886af93a59493ff
DESKTOP-TCOQQLT\jonat <jonathanmounsif@utexas.edu>
2019-04-14 02:11:46 -0500

    Updated company search page, shows company logo and uses different font sizes for the different field

--- ResYouMe/app/src/main/java/com/example/resyoume/CompanySearchActivity.java
+++ ResYouMe/app/src/main/java/com/example/resyoume/CompanySearchActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -14,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,12 +30,28 @@ public class CompanySearchActivity extends AppCompatActivity {
 
     EditText CompanyDomainName;
     TextView CompanyInfo_TextView;
+    TextView CompanyName_Textview;
+    TextView CompanyBio_Textview;
+    TextView CompanyLinkedIn_Textview;
+    TextView CompanyTwitter_Textview;
+    TextView CompanyWebsite_Textview;
+    TextView CompanyLocation_Textview;
+    ImageView CompanyLogo_ImageView;
+
+    String testLogo = "https://img.fullcontact.com/static/a375e4f9a528272b52f38511a5de88b0_d2f5b2872106a596afe1dcf27ffaf920956e91cdd7c024e2fb707377eb6113a3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_info);
         CompanyDomainName = (EditText) findViewById(R.id.CompanyDomainName);
         CompanyInfo_TextView = (TextView) findViewById(R.id.CompanyInfo);
+        CompanyName_Textview = (TextView) findViewById(R.id.CompanyName);
+        CompanyBio_Textview = (TextView) findViewById(R.id.CompanyBio);
+        CompanyLinkedIn_Textview = (TextView) findViewById(R.id.CompanyLinkedIn);
+        CompanyTwitter_Textview = (TextView) findViewById(R.id.CompanyTwitter);
+        CompanyWebsite_Textview = (TextView) findViewById(R.id.CompanyWebSite);
+        CompanyLocation_Textview= (TextView) findViewById(R.id.CompanyLocation);
+        CompanyLogo_ImageView = (ImageView) findViewById(R.id.CompanyLogo);
     }
 
     public void onClickInfo(View view) throws NullPointerException {
@@ -50,7 +69,7 @@ public class CompanySearchActivity extends AppCompatActivity {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    String CompanyName = null, Location = null, Twitter = null, Website = null, Bio = null, Linkedin = null;
+                    String CompanyName = null, Location = null, Twitter = null, Website = null, Bio = null, Linkedin = null, Logo = null;
                     try {
                         CompanyName = response.getString("name");
                         Location = response.getString("location");
@@ -58,21 +77,37 @@ public class CompanySearchActivity extends AppCompatActivity {
                         Website = response.getString("website");
                         Bio = response.getString("bio");
                         Linkedin = response.getString("linkedin");
+                        Logo = response.getString("logo");
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    CompanyInfo_TextView.setText("Company Name: " + CompanyName +
-                            "\nCompany Location: " + Location +
-                            "\nCompany Bio: " + Bio +
-                            "\nCompany Website: " + Website +
-                            "\nCompany Linkedin: " + Linkedin +
-                            "\nCompany Twitter: " + Twitter);
+                    CompanyInfo_TextView.setText("Company Found!");
+                    CompanyName_Textview.setText(CompanyName);
+                    CompanyLocation_Textview.setText(Location);
+                    CompanyWebsite_Textview.setText(Website);
+                    CompanyTwitter_Textview.setText(Twitter);
+                    CompanyLinkedIn_Textview.setText(Linkedin);
+                    CompanyBio_Textview.setText(Bio);
+
+                    Picasso.with(getApplicationContext()).load(Logo).placeholder(R.mipmap.ic_launcher)
+                            .error(R.mipmap.ic_launcher)
+                            .into(CompanyLogo_ImageView, new com.squareup.picasso.Callback() {
+                        @Override
+                        public void onSuccess() {
+                            Toast.makeText(getApplicationContext(), "Image Loaded!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError() {
+                            Toast.makeText(getApplicationContext(), "Could not load image!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     CompanyInfo_TextView.setText("Invalid URL. Please check the spelling and try again.");
-                    //onBackPressed();
                 }
             }) {
                 @Override
