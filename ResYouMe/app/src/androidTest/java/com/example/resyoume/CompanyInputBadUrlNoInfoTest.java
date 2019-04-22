fd47edab0c60c654675e24f35bdca2c0cab8d32a
DESKTOP-TCOQQLT\jonat <jonathanmounsif@utexas.edu>
2019-04-22 02:57:31 -0500

    Added Espresso tests for company search and company input

---
+++ ResYouMe/app/src/androidTest/java/com/example/resyoume/CompanyInputBadUrlNoInfoTest.java
@@ -0,0 +1,95 @@
+package com.example.resyoume;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CompanyInputBadUrlNoInfoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void companyInputBadUrlNoInfoTest() {
+        ViewInteraction textInputEditText = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.text_input_URL),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText.perform(click());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.text_input_URL),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText2.perform(replaceText("bad"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(withText("bad"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.text_input_URL),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText3.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withText("Save"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
